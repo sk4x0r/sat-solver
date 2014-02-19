cc6e86a848e1f4426baa4a5967408f3eff84a3de
sk4x0r <santosh.patil123@gmail.com>
2014-02-19 19:14:39 +0530

    Added unit propogation rule and pure literal rule

--- gbl.py
+++ gbl.py
@@ -3,17 +3,16 @@ import Queue
 import copy
 import sys
 import time
+#TODO: represent terms/formulae using sets instead of lists
 
-GLOBAL_DEBUG=False
-def debug(*s):
-	if(GLOBAL_DEBUG):
-		print s
 
+#Recursively copies the content of list and returns copied list
 def copyList(l):
 	return copy.deepcopy(l)
 
-	
+
 #Generates set of orthonormal terms, given a list of variables
+#This method returns list of `n+1` ON terms for `n` unknowns
 def generateONSet(unknowns):
 	ONSet=[]
 	minCount=min(unknowns)
@@ -32,8 +31,11 @@ def generateONSet(unknowns):
 def findUnassignedVars(term, varList):
 	return [x for x in varList if x not in term and -x not in term]
 
+
 def evaluateQuotient(formula, assignment):
-	
+	#print "evaluateQuotient"
+	#print "formula before", formula
+	#print "assignment", assignment
 	fIter=copyList(formula)
 	for literal in assignment:
 		for term in fIter:
@@ -44,6 +46,7 @@ def evaluateQuotient(formula, assignment):
 			if (-1)*literal in term:
 				formula.remove(term)
 		fIter=copyList(formula)
+	#print "new formula", formula
 	return formula
 
 
@@ -66,11 +69,51 @@ def checkSolution(formula, assignments):
 		return True
 	return False
 
-FLAG=0
 q=Queue.Queue()
-	
+
+
+def applyUnitPropogation(formula, assignments, unknowns):
+	fIter=copyList(formula)
+	alreadyRemoved=[]
+	for term in fIter:
+		if len(term)==1:
+			if abs(term[0]) in alreadyRemoved:
+				continue
+			alreadyRemoved.append(abs(term[0]))
+			assignments+=term
+			unknowns.remove(abs(term[0]))
+			formula=evaluateQuotient(formula, [-1*term[0]])
+	return formula, assignments, unknowns
+
+def applyPureLiteral(formula, assignments, unknowns):
+	for literal in unknowns:
+		literalExists=False
+		complementExists=False
+		for term in formula:
+			if literal in term:
+				literalExists=True
+				if complementExists:
+					break
+			if -1*literal in term:
+				complementExists=True
+				if literalExists:
+					break
+		if literalExists and not complementExists:
+			#print "UnitLiteral"
+			formula=evaluateQuotient(formula, [-1*literal])
+			assignments.append(-1*literal)
+			unknowns.remove(literal)
+			
+		if not literalExists and complementExists:
+			#print "UnitComplement"
+			formula=evaluateQuotient(formula, [literal])
+			assignments.append(literal)
+			unknowns.remove(literal)
+	return formula, assignments, unknowns
+
 def newAlgo(orig_formula, unknowns):
 	global start_time
+	FLAG=0
 	start_time = time.time()
 	formula=copyList(orig_formula)
 	assignments=[]
@@ -81,15 +124,30 @@ def newAlgo(orig_formula, unknowns):
 		formula=t[0]
 		assignments=t[1]
 		unknowns=t[2]
+				
+		formula, assignments, unknowns = applyUnitPropogation(formula, assignments, unknowns)
+		if checkUnsatisfiable(formula):
+			continue
+		#TODO:apply pure literal elimination
+		formula, assignments, unknowns = applyPureLiteral(formula, assignments, unknowns)
+		
+		if len(formula)==0:
+			#print checkSolution(copyList(orig_formula), assignments + onTerm) 
+			FLAG=1
+			print "Solution:",assignments
+			print time.time() - start_time, "seconds"
+			continue
+			#sys.exit(0)
 		onSet=generateONSet(unknowns)
+		
 		for onTerm in onSet:
 			f=evaluateQuotient(copyList(formula), onTerm)
 			if len(f)==0:
-				print checkSolution(copyList(orig_formula), assignments + onTerm) 
+				#print checkSolution(copyList(orig_formula), assignments + onTerm) 
 				FLAG=1
 				print "Solution:",assignments+onTerm
 				print time.time() - start_time, "seconds"
-				sys.exit(0)
+				#sys.exit(0)
 			elif checkUnsatisfiable(f):
 				continue
 			else:
